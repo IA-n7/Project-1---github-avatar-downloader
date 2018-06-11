3674c0233fde55900fe82a5d66537ff4288db47d
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-11 21:25:08 +0000

    Properly downloads all avatars, stores them in files based on login name in Avatar directory

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,11 @@
+
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+
 var request = require('request');
 var githubToken = require("./secrets.js");
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,21 +14,42 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': githubToken.GITHUB_TOKEN
+      'Authorization': "token " + githubToken.GITHUB_TOKEN
     }
   };
 
-  request(options.url, function(err, res, body) {
-    JSON.parse(body);
-    cb(err, body);
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 }
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
 
-HELLO
+function downloadImageByURL(url, filePath) {
 
-  // if (!error && response.statusCod ==)
+  request.get(url)
+     .on('error', function (err) {
+       throw err; 
+     })
+
+     .on('response', function (response) {
+       console.log("Downloading image: ");
+     })
+
+     .on('end', function() {
+      console.log('Download complete.');
+    })
+
+    .pipe(fs.createWriteStream(filePath)
+    );
+}
+
+function callback (err, result) {
+  for (var i = 0; i < result.length; i++) {
+    downloadImageByURL(result[i]["avatar_url"], "./avatars/" + result[i]["login"]);
+  }
+  console.log("Errors: ", err);
+
+}
+
+  getRepoContributors(owner, repo, callback);
